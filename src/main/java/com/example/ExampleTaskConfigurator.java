3de35385c10c5b14c8628c62585bfe3d08f874b1
HOTS\HOTS <hots@hots.com>
2017-08-28 13:14:09 -0400

    workingparam

--- src/main/java/com/example/ExampleTaskConfigurator.java
+++ src/main/java/com/example/ExampleTaskConfigurator.java
@@ -1,21 +1,27 @@
 package com.example;
 
 import com.atlassian.bamboo.task.AbstractTaskConfigurator;
-import com.atlassian.bamboo.task.ActionParametersMap;
+import com.atlassian.bamboo.collections.ActionParametersMap;
 import com.atlassian.bamboo.task.TaskDefinition;
 import com.atlassian.bamboo.task.TaskConfiguratorHelper;
 import java.util.*;  
 
 public class ExampleTaskConfigurator extends AbstractTaskConfigurator {
-    //TODO (save / validation logic)
-@NotNull
-@Override
-public Map<String, String> generateTaskConfigMap(@NotNull ActionParametersMap params, @Nullable TaskDefinition previousTaskDefinition)
-    {
-	final Map<String, String> config = super.generateTaskConfigMap(params, previousTaskDefinition);
-	TaskConfiguratorHelper taskConfiguratorHelper = (TaskConfiguratorHelper) ContainerManager.getInstance().getContainerContext().getComponent("taskConfiguratorHelper");
-	taskConfiguratorHelper.populateTaskConfigMapWithActionParameters(config, params, FIELDS);
-	return config;
-    }
-    
+
+	private String[] fields = new String[]{
+			"environment",
+			"deploymentProject"
+	};
+
+	//TODO (save / validation logic)
+	@Override
+	public Map<String, String> generateTaskConfigMap(final ActionParametersMap params, final TaskDefinition previousTaskDefinition) {
+		final Map<String, String> config = super.generateTaskConfigMap(params, previousTaskDefinition);
+
+		for (String field : fields) {
+			config.put(field, params.getString(field));
+		}
+
+		return config;
+	}
 }
